d0ac35b9232736340f32ed8b6e9fc9ecc20a51dc
Ahmed Nafies <ahmed.nafies@gmail.com>
2021-11-19 00:12:12 +0100

    removes assets

--- src/Components/ImageCard/ImageCard.component.jsx
+++ src/Components/ImageCard/ImageCard.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import { FaBook } from "react-icons/fa";
--- src/Pages/Home/Home.page.jsx
+++ src/Pages/Home/Home.page.jsx
@@ -9,21 +9,21 @@ const books = [
         subheader: 'by Steve McConnell',
         cover: 'https://images.gr-assets.com/books/1396837641l/4845.jpg',
         stars: 4,
-        url: "src/Assets/AirForce.usdz",
+        url: "https://devimages-cdn.apple.com/ar/photogrammetry/AirForce.usdz",
     },
     {
         title: 'Clean Code',
         subheader: 'by Robert Cecil Martin',
         cover: 'https://images.gr-assets.com/books/1436202607l/3735293.jpg',
         stars: 5,
-        url: "https://www.goodreads.com/book/show/3735293-clean-code"
+        url: "https://devimages-cdn.apple.com/ar/photogrammetry/FruitCakeSlice.usdz"
     },
     {
         title: 'Working Effectively with Legacy Code',
         subheader: 'by Michael Feathers',
         cover: 'https://images.gr-assets.com/books/1348627451l/44919.jpg',
         stars: 4,
-        url: "https://www.goodreads.com/book/show/44919.Working_Effectively_with_Legacy_Code",
+        url: "https://devimages-cdn.apple.com/ar/photogrammetry/LemonMeringuePie.usdz",
     }
 ]
 
@@ -31,7 +31,7 @@ const books = [
 function Books() {
     return (
         <div>
-            <Grid  sx={{ flexGrow: 1 }} container spacing={24}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                 <Grid item xs={12}>
                     <div>
                         <h1>My Favorite Items</h1>
@@ -41,7 +41,7 @@ function Books() {
                     return <Slide direction="left" in={true} key={index}
                         style={{ transitionDelay: index * 50 }}
                         mountOnEnter unmountOnExit>
-                        <Grid item xs={12}>
+                        <Grid item xs={12} md={4}>
                             <ImageCard card={book} index={index} />
                         </Grid>
                     </Slide>
